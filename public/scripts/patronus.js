0bb51da92ffa801d1c31f682e8cdec589c7568ef
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-20 14:31:58 -0500

    modified patronus.js and people.js to handle clicks and log response data

--- public/scripts/patronus.js
+++ public/scripts/patronus.js
@@ -1,6 +1,18 @@
 angular.module('harryApp', []);
 angular.module('harryApp').controller('PatronusController', function($scope,$http){
 
+  $scope.handleClick = function() {
+    $http.get('/patronus')
+    .then(function(response){
+      console.log(response);
+    });
+  };
 
+  $scope.handleClick2 = function() {
+    $http.post('/patronus')
+    .then(function(response){
+        console.log(response);
+    });
+  };
 
-})
+});
--- public/scripts/people.js
+++ public/scripts/people.js
@@ -1,6 +1,22 @@
 angular.module('harryApp', []);
 angular.module('harryApp').controller('PeopleController', function($scope,$http){
 
+  $scope.handleClick = function() {
+    $http.get('/people')
+    .then(function(response){
+      console.log(response);
+    });
+  };
+
+  $scope.handleClick2 = function() {
+    $http.post('/people')
+    .then(function(response){
+        console.log(response);
+    });
+  };
+});
+
+
 
 
 })
